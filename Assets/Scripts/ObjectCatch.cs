cb98c63310ee161b45bd5306cc3bb303dcca7dcf
Rodrigo Sāmietis <samietisrodrigo@gmail.com>
2026-02-18 08:25:19 +0200

    Pabeidzu programmu

--- Assets/Scripts/ObjectCatch.cs
+++ Assets/Scripts/ObjectCatch.cs
@@ -1,11 +1,17 @@
+using TMPro;
 using UnityEngine;
 
 public class ObjectCatch : MonoBehaviour
 {
     public float sizeIncrease = 0.5f;
     public float massIncrease = 1f;
+    public TMP_Text counterText;
+
+    public int eatenD = 0;
     private Rigidbody2D rb;
     SFXScript sfx;
+    [SerializeField] private playerLife playerLife;
+
 
 
     void Start()
@@ -26,13 +32,16 @@ public class ObjectCatch : MonoBehaviour
             Destroy(collision.gameObject);
             transform.localScale += new Vector3(sizeIncrease, sizeIncrease, 0);
             rb.mass += massIncrease;
+            eatenD += 100;
+            counterText.text = "Points:\n" + eatenD;
 
         }
         else if (collision.CompareTag("Enemy")) {
-            sfx.PlaySFX(4);
+            sfx.PlaySFX(5);
+            playerLife.removeLife();
             Destroy(collision.gameObject);
-            transform.localScale += new Vector3(sizeIncrease, sizeIncrease, 0);
-            rb.mass += massIncrease;
+
+           
         }
     }
 
--- Assets/Scripts/playerLife.cs
+++ Assets/Scripts/playerLife.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerLife : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject[] lifes;
+    public GameObject player;
+    public Toggle gameToggle;
+
+    [SerializeField]private ObjectCatch ObjectCatch;
+    private int amoutOfLifes = 3;
+
+    public void resetLife()
     {
-        
+        for (int i = 0; i < lifes.Length; i++)
+        {
+            lifes[i].SetActive(true);
+            ObjectCatch.eatenD = 0;
+            player.transform.localScale = new Vector3(1f, 1f, 1f);
+            amoutOfLifes = 3;
+        }
+    }
+    public void removeLife(){
+        amoutOfLifes--;
+
+        lifes[amoutOfLifes].SetActive(false);
+
+        if (!lifes[0].activeSelf)
+        {
+            gameToggle.isOn = false;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
